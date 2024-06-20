76017368b909e91ff0b22b2098a1d9bc8d5273fa
Platon61633 <peredreevplaton@gmail.com>
2024-06-20 18:51:04 +0300

    gf

--- api/index.php
+++ api/index.php
@@ -6,48 +6,84 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($_GET['showcase']) {
     case 'key':
+
+        class Item {
+            public $id;
+            public $name;
+            public $desc;
+            public $type;
+            public $brand;
+            public $country;
+            public $price;
+
+            function __construct($id, $name, $desc, $type, $brand, $country, $price) {
+                $this ->id = $id;
+                $this ->name = $name;
+                $this ->desc = $desc;
+                $this ->type = $type;
+                $this ->brand = $brand;
+                $this ->country = $country;
+                $this ->price = $price;
+            }
+        }
+        
         $arr = [
-            'id' => 123,
+            ['id' => 123,
             'name' => 'jora',
             'desc' => 'bespolezniy',
             'type' => 'trash',
             'brand' => 'epic',
             'country' => 'gacu',
-            'price' => 613,
-            'id' => 123,
+            'price' => 613],
+            ['id' => 123,
             'name' => 'jora',
             'desc' => 'bespolezniy',
             'type' => 'trash',
             'brand' => 'epic',
             'country' => 'gacu',
-            'id' => 123,
+            'price' =>812],
+            ['id' => 123,
             'name' => 'jora',
             'desc' => 'bespolezniy',
             'type' => 'trash',
             'brand' => 'epic',
             'country' => 'gacu',
-            'price' => 613,
-            'id' => 123,
+            'price' => 613],
+            ['id' => 123,
             'name' => 'jora',
             'desc' => 'bespolezniy',
             'type' => 'trash',
             'brand' => 'epic',
             'country' => 'gacu',
-            'id' => 123,
+            'price' =>812],
+            ['id' => 123,
             'name' => 'jora',
             'desc' => 'bespolezniy',
             'type' => 'trash',
             'brand' => 'epic',
             'country' => 'gacu',
-            'price' => 613,
-            'id' => 123,
+            'price' => 613],
+            ['id' => 123,
             'name' => 'jora',
             'desc' => 'bespolezniy',
             'type' => 'trash',
             'brand' => 'epic',
             'country' => 'gacu',
+            'price' =>812],
         ];
-        echo json_encode($arr);
+
+        $arr2 = [
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+                new Item(123, 'jora', 'bespolezniy', 'trash', 'epic', 'gacu', 613),
+            ];
+        ]
+        echo json_encode($arr2);
         break;
     
     default:
